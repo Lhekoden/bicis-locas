6a80edbb83044bb8afc2b76b6e36c5bf287747fe
Lhekoden <nikjoafre@hotmail.com>
2020-12-18 14:20:59 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-
+	/* 1. Traemos los valores de los campos del formulario */
+	var name = $('#name').val();
+	console.log(name);
+	var lastname = $('#lastname').val();
+	console.log(lastname);
+	var input_email = $('#input-email').val();
+	console.log(input_email);
+	var input_password = $('#input-password').val();
+	console.log(input_password);
+	
 }
